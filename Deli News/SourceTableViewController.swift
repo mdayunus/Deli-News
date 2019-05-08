dafa47c13bd442c6b449dba7a2b1e740819c1837
mdayunus <mdayunus@gmail.com>
2019-05-09 00:39:19 +0530

    intiial setup

---
+++ Deli News/SourceTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SourceTableViewController.swift
+//  Deli News
+//
+//  Created by Mohammad Yunus on 09/05/19.
+//  Copyright © 2019 taptap. All rights reserved.
+//
+
+import UIKit
+
+class SourceTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}
